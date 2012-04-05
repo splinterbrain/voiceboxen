e1f7105492f0feec99c1da3da7bd226329709214
splinterbrain <splinterbrain@gmail.com>
2012-04-04 21:31:11 -0700

    Basic song pushing

--- js/models.js
+++ js/models.js
@@ -3,13 +3,14 @@ $(function() {
 	window.VOICEBOXEN = {};
 	VOICEBOXEN.PARSE_APP_ID = "wr0JaM2SXMbwTX1Q142F8lFI29elxoPYjE768BEB";
 	VOICEBOXEN.PARSE_API_KEY = "HpQBA0WQ2Cxl8FFoGk5QKP3h33wL9LVPGiuwRtJ1";
+	VOICEBOXEN.PARSE_HEADERS = {
+					"X-Parse-Application-Id" : VOICEBOXEN.PARSE_APP_ID,
+					"X-Parse-REST-API-Key" : VOICEBOXEN.PARSE_API_KEY
+			};
 
 	VOICEBOXEN.ParseSync = function(method, model, options) {
 		return Backbone.sync(method, model, _.extend(options, {
-			headers : {
-				"X-Parse-Application-Id" : VOICEBOXEN.PARSE_APP_ID,
-				"X-Parse-REST-API-Key" : VOICEBOXEN.PARSE_API_KEY
-			}
+			headers : VOICEBOXEN.PARSE_HEADERS
 		}));
 	};
 
@@ -34,17 +35,27 @@ $(function() {
 				}),
 				contentType : "application/json",
 				dataType : "json",
-				headers : {
-					"X-Parse-Application-Id" : VOICEBOXEN.PARSE_APP_ID,
-					"X-Parse-REST-API-Key" : VOICEBOXEN.PARSE_API_KEY
-				},
+				headers : VOICEBOXEN.PARSE_HEADERS,
 				success : $.proxy(function(resp) {
 					var user = new VOICEBOXEN.User(resp);
 					this.user = user;
+					if(this.cbFunction) {
+						var cbFunction = this.cbFunction;
+						var cbContext = this.cbContext;
+						this.cbFunction = null;
+						this.cbContext = null;
+						$.proxy(cbFunction, cbContext);
+					}
+
 				}, this)
 			}).error(function(resp) {
 				alert("Sorry, there was a problem creating that user");
 			});
+		},
+		presentLogin : function(cbFunction, cbContext) {
+			this.$el.find("#popup").addClass("visible");
+			this.cbFunction = cbFunction;
+			this.cbContext = cbContext;
 		},
 		login : function() {
 			$.ajax({
@@ -55,13 +66,19 @@ $(function() {
 					password : this.$el.find("#password").val()
 				},
 				dataType : "json",
-				headers : {
-					"X-Parse-Application-Id" : VOICEBOXEN.PARSE_APP_ID,
-					"X-Parse-REST-API-Key" : VOICEBOXEN.PARSE_API_KEY
-				},
+				headers : VOICEBOXEN.PARSE_HEADERS,
 				success : $.proxy(function(resp) {
 					var user = new VOICEBOXEN.User(resp);
 					this.user = user;
+					this.$el.find("#popup").removeClass("visible");
+					if(this.cbFunction) {
+						var cbFunction = this.cbFunction;
+						var cbContext = this.cbContext;
+						this.cbFunction = null;
+						this.cbContext = null;
+						$.proxy(cbFunction, cbContext);
+					}
+
 				}, this)
 			}).error(function(resp) {
 				alert("Sorry, there was a problem logging in that user");
@@ -95,11 +112,14 @@ $(function() {
 	VoiceBoxen.user = null;
 
 	VOICEBOXEN.User = Backbone.Model.extend({
-		
+		idAttribute : "objectId"
+
 	});
 
 	VOICEBOXEN.Song = Backbone.Model.extend({
 		sync : VOICEBOXEN.ParseSync,
+		idAttribute : "objectId",
+		url : "https://api.parse.com/1/classes/Song",
 
 		initialize : function(attr) {
 			if(isNaN(attr.vbid) && !isNaN(attr.id)) {
@@ -108,6 +128,23 @@ $(function() {
 					vbid : attr.id
 				});
 			}
+
+			$.ajax({
+				url : this.url,
+				type : "GET",
+				headers : VOICEBOXEN.PARSE_HEADERS,
+				data : {
+					where : "{\"vbid\" : " + this.get("vbid") + "}"
+				},
+				success : $.proxy(function(resp) {
+					if(resp.results.length > 0) {
+						this.set({
+							objectId : resp.results[0].objectId
+						});
+					}
+				}, this)
+			});
+
 		},
 		parse : function(resp) {
 			if(isNaN(resp.vbid) && !isNaN(resp.id)) {
@@ -164,6 +201,38 @@ $(function() {
 			}, this));
 		},
 		singLater : function() {
+			if(VoiceBoxen.user == null) {
+				VoiceBoxen.presentLogin(this.singLater, this);
+				return;
+			}
+
+			//Save song
+			this.model.save({}, {
+				success : $.proxy(function(model, resp) {
+					//Save song to user
+					$.ajax({
+						url : "https://api.parse.com/1/classes/UserSongKey",
+						type : "POST",
+						headers : VOICEBOXEN.PARSE_HEADERS,
+						data : {
+							user : {
+								__type : "Pointer",
+								className : "_User",
+								objectId : VoiceBoxen.user.get("id")
+							},
+							song : {
+								__type : "Pointer",
+								className : "Song",
+								objectId : this.model.get("id")
+							}
+						},
+						dataType : "json",
+						success : function(resp) {
+
+						}
+					});
+				}, this)
+			});
 
 		}
 	});
